52d5d2f076b0998f8a19b8af240d3ed65951a5f0
Flavio Henrique <flaviohenriqu@gmail.com>
2015-03-30 23:07:02 -0300

    add import_export Products, Labels and Issues, and link session list

--- exploratory/admin.py
+++ exploratory/admin.py
@@ -14,9 +14,35 @@ class SessaoAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     search_fields = ('tester', 'charter', 'comments')
     list_filter = ('tester', )
     list_display = ('tester', 'charter', 'product', 'comments')
+    list_display_links = ('tester', )
+
+class LabelResource(resources.ModelResource):
+    class Meta:
+        model = Label
+    pass
+
+class LabelAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    resources = LabelResource
+
+class ProductResource(resources.ModelResource):
+    class Meta:
+        model = Product
+    pass
+
+class ProductAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    resources = ProductResource
+    list_display = ('name', 'project')
+
+class IssueResource(resources.ModelResource):
+    class Meta:
+        model = Issue
+    pass
+
+class IssueAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    resources = IssueResource
 
 # Register your models here.
 admin.site.register(Sessao, SessaoAdmin)
-admin.site.register(Product)
-admin.site.register(Label)
-admin.site.register(Issue)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Label, LabelAdmin)
+admin.site.register(Issue, IssueAdmin)
